f630671444e10a2b99b2900a81d7dd750eebe1fe
Luis Balderas Ruiz <luisbalru@ugr.es>
2025-09-24 06:38:52 +0200

    main test file

---
+++ main.py
@@ -0,0 +1,7 @@
+from MoEITS_simplification_service import MoEITS_Simplification_Service
+import numpy as np
+
+moe_simp_service = MoEITS_Simplification_Service("deepseek-ai/deepseek-moe-16b-base")
+print("Parámetros modelo original: ", np.sum(p.numel() for p in moe_simp_service.original_model.parameters() if p.requires_grad))
+input()
+print(moe_simp_service.original_model)
