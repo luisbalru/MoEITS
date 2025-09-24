1494fa7dc25bf8d135e2eebe0915f803517d901c
Luis Balderas Ruiz <luisbalru@ugr.es>
2025-09-24 19:15:26 +0200

    printing experts

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import numpy as np
 moe_simp_service = MoEITS_Simplification_Service("deepseek-ai/deepseek-moe-16b-base")
 print("Parámetros modelo original: ", np.sum(p.numel() for p in moe_simp_service.original_model.parameters() if p.requires_grad))
 input()
-print(moe_simp_service.original_model)
+print(moe_simp_service.original_model.layers[1].mlp)
