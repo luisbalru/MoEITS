6789881beef8f0ccb370ffd0c9d39a48d9a65b4e
Luis Balderas Ruiz <luisbalru@ugr.es>
2025-09-24 06:38:41 +0200

    moe simplification service. Just init

---
+++ MoEITS_simplification_service.py
@@ -0,0 +1,23 @@
+import numpy as np
+from scipy.stats import entropy, iqr
+import pandas as pd
+from transformers import AutoTokenizer, MixtralForCausalLM, MixtralConfig, AutoModelForCausalLM
+import os
+from numpy import unravel_index
+import torch
+import json
+
+
+class MoEITS_Simplification_Service:
+
+    def __init__(self, model_name, output_base_path='', config_path='utils/config.json'):
+        with open(config_path, 'r') as f:
+            config = json.load(f)
+        self.model_name = model_name
+        self.output_base_path = output_base_path
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name, token=config['token'])
+        self.original_model = AutoModelForCausalLM.from_pretrained(self.model_name, token=config['token'], trust_remote_code=True, torch_dtype="auto")
+        self.simplified_model = None
+        self.layers = {}
+
+
