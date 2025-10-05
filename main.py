a512c827db2a9c4cd5c6b4d9e9054d6ce6514463
Luis Balderas Ruiz <luisbalru@ugr.es>
2025-10-05 13:22:42 +0200

    probando deepseekv3

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 from Mixtral8x7b_simplification_service import Mixtral8x7b_Simplification_Service
 from DeepSeekMoE_simplification_service import DeepSeekMoE_Simplification_Service
 import numpy as np
+from transformers import AutoTokenizer, MixtralForCausalLM, MixtralConfig, AutoModelForCausalL, DeepseekV3ForCausalLM
 
+"""
 #moe_simp_service = Mixtral8x7b_Simplification_Service("mistralai/Mixtral-8x7B-Instruct-v0.1")
 moe_simp_service = DeepSeekMoE_Simplification_Service("deepseek-ai/deepseek-moe-16b-base")
 print("Parámetros modelo original: ", np.sum(p.numel() for p in moe_simp_service.original_model.parameters() if p.requires_grad))
@@ -10,3 +12,6 @@ simp = moe_simp_service.simplify_original_model()
 print("Parámetros modelo simplificado: ", np.sum(p.numel() for p in simp.parameters() if p.requires_grad))
 input()
 print(simp)
+"""
+ds = DeepseekV3ForCausalLM()
+print(ds)
