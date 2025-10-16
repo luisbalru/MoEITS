9853be5f064d696fe09ce18979501df223eebcec
Luis Balderas Ruiz <luisbalru@ugr.es>
2025-10-16 19:03:27 +0200

    new experiment with factor 3

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ def count_trainable_parameters(model):
 
 
 if __name__ == '__main__':
-    deepseek_simp_service = DeepSeekMoE_Simplification_Service('deepseek-ai/deepseek-moe-16b-base')
+    deepseek_simp_service = DeepSeekMoE_Simplification_Service('deepseek-ai/deepseek-moe-16b-base', factor=3)
     simplified_deepseek_moe = deepseek_simp_service.simplify_original_model()
     print(simplified_deepseek_moe)
     print(f"Params: {count_trainable_parameters(simplified_deepseek_moe)}")
-    simplified_deepseek_moe.save_pretrained('simplified_models/deepseek-moe-16b/')
+    simplified_deepseek_moe.save_pretrained('simplified_models/deepseek-moe-16b-f3/')
