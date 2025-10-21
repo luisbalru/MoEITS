9b2c6217b16b066a12256117b9f3e096a3cb5eed
Luis Balderas Ruiz <luisbalru@ugr.es>
2025-10-21 20:12:34 +0200

    GenerationMixin

--- models/deepseek_moe_16b/modeling_deepseek.py
+++ models/deepseek_moe_16b/modeling_deepseek.py
@@ -856,7 +856,8 @@ class DeepseekSdpaAttention(DeepseekAttention):
 
         kv_seq_len = key_states.shape[-2]
         if past_key_value is not None:
-            kv_seq_len += past_key_value.get_usable_length(kv_seq_len, self.layer_idx)
+            kv_seq_len = past_key_value.get_seq_length()
+            #kv_seq_len += past_key_value.get_usable_length(kv_seq_len, self.layer_idx)
         cos, sin = self.rotary_emb(value_states, seq_len=kv_seq_len)
 
         query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
@@ -1176,7 +1177,8 @@ class DeepseekModel(DeepseekPreTrainedModel):
             use_legacy_cache = not isinstance(past_key_values, Cache)
             if use_legacy_cache:
                 past_key_values = DynamicCache.from_legacy_cache(past_key_values)
-            past_key_values_length = past_key_values.get_usable_length(seq_length)
+            past_key_values_length = past_key_values.get_seq_length()
+            #past_key_values_length = past_key_values.get_usable_length(seq_length)
 
         if position_ids is None:
             device = input_ids.device if input_ids is not None else inputs_embeds.device
