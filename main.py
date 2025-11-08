afe62ebb6b11dca9b0cca166e5c6fa90c494567a
Luis Balderas Ruiz <luisbalru@ugr.es>
2025-11-08 12:02:09 +0100

    adding deepseekv2lite sipmlification service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Mixtral8x7b_simplification_service import Mixtral8x7b_Simplification_Service
 from DeepSeekMoE_simplification_service import DeepSeekMoE_Simplification_Service
+from DeepSeekV2Lite_simplification_service import DeepSeekV2Lite_Simplification_Service
 import numpy as np
 import sys
 
@@ -15,10 +16,10 @@ if __name__ == '__main__':
     factor = float(sys.argv[2])
 
     if 'deepseek' in model_name:
-        deepseek_simp_service = DeepSeekMoE_Simplification_Service('deepseek-ai/deepseek-moe-16b-base', factor=factor)
+        deepseek_simp_service = DeepSeekV2Lite_Simplification_Service("deepseek-ai/DeepSeek-V2-Lite", factor=factor)
         simplified_deepseek_moe = deepseek_simp_service.simplify_original_model()
         print(f"Params: {count_trainable_parameters(simplified_deepseek_moe)}")
-        simplified_deepseek_moe.save_pretrained(f'simplified_models/deepseek-moe-16b-f{factor}/')
+        simplified_deepseek_moe.save_pretrained(f'simplified_models/deepseek-v2-lite-f{factor}/')
     elif 'mixtral' in model_name:
         mixtral_simp_service = Mixtral8x7b_Simplification_Service('mistralai/Mixtral-8x7B-v0.1', factor=factor)
         simplified_mixtral = mixtral_simp_service.simplify_original_model()
