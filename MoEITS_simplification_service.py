499da6e7aa303ab74ee4099a1e7ff0d7acf63bd9
Luis Balderas Ruiz <luisbalru@ugr.es>
2025-11-16 19:48:33 +0100

    adjusting to 44 experts per layer max

--- MoEITS_simplification_service.py
+++ MoEITS_simplification_service.py
@@ -70,7 +70,7 @@ class MoEITS_Simplification_Service(ABC):
         """
         #Simulation
         print("Simulating pruning process...")
-        num_experts = np.random.randint(1, 60, size=26).tolist()
+        num_experts = np.random.randint(1, 44, size=26).tolist()
         name_experts = []
         for n in num_experts:
             name_experts.append(list(range(0, n)))
