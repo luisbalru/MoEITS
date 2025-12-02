41de5b96b45bb88dbeb828ac7858e92eb0ec23d6
luisbalru <balderasruizluis@gmail.com>
2025-12-02 18:17:11 +0100

    attn implementation eager

--- evals/MoEITS_evaluation.py
+++ evals/MoEITS_evaluation.py
@@ -22,7 +22,7 @@ class MoEITSEvaluation(DeepEvalBaseLLM):
             device_map="auto",
             trust_remote_code=True,
             torch_dtype=torch.float16,
-            #attn_implementation="flash_attention_2" if torch.cuda.is_available() else "eager"
+            attn_implementation="eager"        
         )
         self.model.eval()
 
