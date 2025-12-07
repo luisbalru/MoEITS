adde3b13aef3c67bba8224de398eb27f348c72f1
Luis Balderas Ruiz <luisbalru@ugr.es>
2025-12-07 19:33:15 +0100

    añadiendo test para mixtral8x7b

--- tests/test_simplification.py
+++ tests/test_simplification.py
@@ -48,3 +48,48 @@ def test_layers_weights_qwen_simplification_service():
 
 
     assert num_layers and same_weights and expert_weights
+
+
+def test_layers_weights_mixtral8x7b_simplification_service():
+    factor = 5
+    simp_service = Mixtral8x7b_Simplification_Service("mistralai/Mixtral-8x7B-Instruct-v0.1", factor=factor)
+    simp_model = simp_service.simplify_original_model(mode='test', name = "qwen")
+    num_layers = len(simp_service.name_experts) == len(simp_model.model.layers)
+
+    or_mod = simp_service.original_model
+    expert_names = simp_service.name_experts
+
+    same_weights = True
+
+    for i in range(0, len(or_mod.model.layers)):
+        names_experts = expert_names[i]
+        same_weights = same_weights and torch.equal(simp_model.model.layers[i].self_attn.q_proj.weight, or_mod.model.layers[i].self_attn.q_proj.weight)
+        same_weights = same_weights and torch.equal(simp_model.model.layers[i].self_attn.k_proj.weight, or_mod.model.layers[i].self_attn.k_proj.weight)
+        same_weights = same_weights and torch.equal(simp_model.model.layers[i].self_attn.v_proj.weight, or_mod.model.layers[i].self_attn.v_proj.weight)
+        same_weights = same_weights and torch.equal(simp_model.model.layers[i].self_attn.o_proj.weight, or_mod.model.layers[i].self_attn.o_proj.weight)
+
+        same_weights = same_weights and torch.equal(simp_model.model.layers[i].block_sparse_moe.gate.weight, or_mod.model.layers[i].block_sparse_moe.gate.weight[names_experts,:])
+        same_weights = same_weights and torch.equal(simp_model.model.layers[i].input_layernorm.weight,or_mod.model.layers[i].input_layernorm.weight)
+        same_weights = same_weights and torch.equal(simp_model.model.layers[i].post_attention_layernorm.weight,or_mod.model.layers[i].post_attention_layernorm.weight)
+ 
+    same_weights = same_weights and torch.equal(simp_model.model.norm.weight,or_mod.model.norm.weight)
+    same_weights = same_weights and torch.equal(simp_model.lm_head.weight,or_mod.lm_head.weight)
+
+    expert_weights = True
+
+    for i in range(0, len(or_mod.model.layers)):
+            names_experts = expert_names[i]
+            for j, e in enumerate(names_experts):
+                expert_weights = expert_weights and torch.equal(simp_model.model.layers[i].mlp.experts[j].w1.weight, or_mod.model.layers[i].mlp.experts[e].w1.weight)
+                expert_weights = expert_weights and torch.equal(simp_model.model.layers[i].mlp.experts[j].w2.weight, or_mod.model.layers[i].mlp.experts[e].w2.weight)
+                expert_weights = expert_weights and torch.equal(simp_model.model.layers[i].mlp.experts[j].w3.weight, or_mod.model.layers[i].mlp.experts[e].w3.weight)
+
+
+    assert num_layers and same_weights and expert_weights
+
+
+
+
+
+#def test_layers_weights_deepseekv2_simplification_service():
+     #pass
