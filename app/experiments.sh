d6a504dff4f02f5287fff657d5caec530ef729f5
Luis Balderas Ruiz <luisbalru@ugr.es>
2025-12-09 17:57:40 +0100

    only deepseek experiments

--- app/experiments.sh
+++ app/experiments.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Mixtral 8x7B Instruct
-python3 main.py mixtral 5 prod
-python3 main.py mixtral 2.5 prod
-python3 main.py mixtral 1.25 prod
+#python3 main.py mixtral 5 prod
+#python3 main.py mixtral 2.5 prod
+#python3 main.py mixtral 1.25 prod
 
 
 
 # Qwen1.5-MoE-A2.7B-Chat
-python3 main.py qwen1.5 5 prod
-python3 main.py qwen1.5 2.5 prod
-python3 main.py qwen1.5 1.25 prod
+#python3 main.py qwen1.5 5 prod
+#python3 main.py qwen1.5 2.5 prod
+#python3 main.py qwen1.5 1.25 prod
 
 
 # DeepSeek-V2-Lite-Chat
