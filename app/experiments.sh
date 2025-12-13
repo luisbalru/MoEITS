6f7c178360f1cd0e07e4663015863f2d55d1f60b
Luis Balderas Ruiz <luisbalru@ugr.es>
2025-12-14 00:19:23 +0100

    printing names

--- app/experiments.sh
+++ app/experiments.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Mixtral 8x7B Instruct
+echo "Mixtral"
 python3 main.py mixtral 7.5 prod
 python3 main.py mixtral 5 prod
 python3 main.py mixtral 2.5 prod
@@ -9,6 +10,7 @@ python3 main.py mixtral 1.25 prod
 
 
 # Qwen1.5-MoE-A2.7B-Chat
+echo "Qwen 1.5"
 python3 main.py qwen1.5 7.5 prod
 python3 main.py qwen1.5 5 prod
 python3 main.py qwen1.5 2.5 prod
@@ -16,6 +18,7 @@ python3 main.py qwen1.5 1.25 prod
 
 
 # DeepSeek-V2-Lite-Chat
+echo "Deepseek"
 python3 main.py deepseek 7.5 prod
 python3 main.py deepseek 5 prod
 python3 main.py deepseek 2.5 prod
