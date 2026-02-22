d507ce9a48a94a3c945e0e6d30103d09b7a07288
Luis Balderas Ruiz <luisbalru@ugr.es>
2026-02-22 01:36:41 +0100

    per_device_train_batch_size=16

--- training/train.py
+++ training/train.py
@@ -215,7 +215,7 @@ def train(model_name, output_dir):
     training_args = TrainingArguments(
         output_dir=output_dir,
         
-        per_device_train_batch_size=32,
+        per_device_train_batch_size=16,
         gradient_accumulation_steps=4,
         
         learning_rate=2e-4,
