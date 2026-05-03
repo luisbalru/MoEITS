eb24d823344e685e8b58685880936e2b7e41a4b9
Luis Balderas Ruiz <luisbalru@ugr.es>
2026-05-03 09:05:38 +0200

    updating main introducing argparse and new arg called num_experts. If both are None, exit

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ from moeits.DeepSeekV2Lite_simplification_service import DeepSeekV2Lite_Simplifi
 from moeits.Qwen2MoE_simplification_service import Qwen2MoE_Simplification_Service
 import numpy as np
 import sys
+import argparse
+
 
 
 def count_trainable_parameters(model):
@@ -13,24 +15,47 @@ def count_trainable_parameters(model):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="MoEITS setup")
 
-    model_name = sys.argv[1]
-    factor = float(sys.argv[2])
-    mode = sys.argv[3]
-    # TODO: COMPROBACIÓN DE PARÁMETROS Y PROPAGACIÓN EN TODOS LOS MODELOS
+    parser.add_argument(
+        "--model-name",
+        choices=["deepseekv2lite", "mixtral8x7B", "qwen1.5"],
+        required=True
+    )
 
-    if 'deepseek' in model_name:
-        deepseek_simp_service = DeepSeekV2Lite_Simplification_Service("deepseek-ai/DeepSeek-V2-Lite-Chat", factor=factor)
+    parser.add_argument(
+        "--mode",
+        choices=["prod", "test"],
+        required=True
+    )
+
+    parser.add_argument("--num-experts", type=int, default=None)
+    parser.add_argument("--factor", type=float, default=None)
+
+
+    args = parser.parse_args()
+
+    model_name = args.model_name
+    factor = args.factor
+    num_experts = args.num_experts
+    mode = args.mode
+
+    if factor == None and num_experts == None:
+        print("Error. Both factor and number of experts cannot be None. Exiting")
+        exit()
+
+    if 'deepseekv2lite' in model_name:
+        deepseek_simp_service = DeepSeekV2Lite_Simplification_Service("deepseek-ai/DeepSeek-V2-Lite-Chat", factor=factor, number_of_experts=num_experts)
         simplified_deepseek_moe = deepseek_simp_service.simplify_original_model(mode=mode, name=model_name)
         print(f"Params: {count_trainable_parameters(simplified_deepseek_moe)}")
         simplified_deepseek_moe.save_pretrained(f'/MoEITS/simplified_models/deepseek-v2-lite-chat-f{factor}-m{mode}/')
-    elif 'mixtral' in model_name:
-        mixtral_simp_service = Mixtral8x7b_Simplification_Service("mistralai/Mixtral-8x7B-Instruct-v0.1", factor=factor)
+    elif 'mixtral8x7B' in model_name:
+        mixtral_simp_service = Mixtral8x7b_Simplification_Service("mistralai/Mixtral-8x7B-Instruct-v0.1", factor=factor, number_of_experts=num_experts)
         simplified_mixtral = mixtral_simp_service.simplify_original_model(mode=mode, name=model_name)
         print(f"Params: {count_trainable_parameters(simplified_mixtral)}")
         simplified_mixtral.save_pretrained(f'/MoEITS/simplified_models/mixtral_8x7b_instruct-f{factor}-m{mode}/')
     elif 'qwen1.5' in model_name:
-        qwen_simp_service = Qwen2MoE_Simplification_Service("Qwen/Qwen1.5-MoE-A2.7B-Chat", factor=factor)
+        qwen_simp_service = Qwen2MoE_Simplification_Service("Qwen/Qwen1.5-MoE-A2.7B-Chat", factor=factor, number_of_experts=num_experts)
         simplified_qwen = qwen_simp_service.simplify_original_model(mode=mode, name=model_name)
         print(f"Params: {count_trainable_parameters(simplified_qwen)}")
         simplified_qwen.save_pretrained(f'/MoEITS/simplified_models/qwen1.5-MoE-A2.7B-Chat-f{factor}-m{mode}/')
